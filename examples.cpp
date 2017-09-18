114ad01a7ff3e136ce3641cc24b8e8c47876c2eb
Anita Aune <aune.anita@gmail.com>
2017-09-18 13:21:49 +0200

    Implemented missing functions

--- examples.cpp
+++ examples.cpp
@@ -1,8 +1,8 @@
 #include "crypto.hpp"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
-// Comment
 
 int main() {
   cout << "SHA-1 with 1 iteration" << endl;
@@ -13,4 +13,28 @@ int main() {
 
   cout << "The derived key from the PBKDF2 algorithm" << endl;
   cout << Crypto::hex(Crypto::pbkdf2("Password", "Salt")) << endl;
+  
+  // Nøkkelen: “ab29d7b5c589e18b52261ecba1d3a7e7cbf212c6”
+  // Saltet: “Saltet til Ola”
+  // Iterasjoner: 2048
+  
+  cout << "\nOppgave 3 Oving 8" << endl;
+  
+  string salt = "Saltet til Ola";
+  string pass;
+
+char passord[3];
+
+
+for(int i = 65; i <= 122; i++) {
+  for(int j = 65; j <= 122; j++) {
+    for(int k = 65; k <= 122; k++) {
+      passord[0] = i;
+      passord[1] = j;
+      passord[2] = k;
+      if (Crypto::hex(Crypto::pbkdf2_2(passord, salt))== "ab29d7b5c589e18b52261ecba1d3a7e7cbf212c6"){
+        cout << "Passordet er: " << passord;
+      }
+    }
+  }
 }
